99a1342b3d00a24e27f9645fd3aba6f6178bce94
Ajaygupta1993 <ajaykumar.gupta04@gmail.com>
2021-07-22 10:01:52 +0530

    Commit for web fonts

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/PasswordEncoderTest.java
@@ -0,0 +1,19 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTest {
+	@Test
+	public void testEncodePassword() {
+		BCryptPasswordEncoder passwordEncoder= new BCryptPasswordEncoder();
+		String rawPassword="AJAY@gupta04";
+		String encodePassword = passwordEncoder.encode(rawPassword);
+		System.out.println(encodePassword);
+		boolean matchPassword = passwordEncoder.matches(rawPassword, encodePassword);
+		assertThat(matchPassword).isTrue();
+	}
+
+}
