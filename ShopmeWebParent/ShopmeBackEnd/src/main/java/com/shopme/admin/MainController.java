2b2bb0821527142424ea828cc45505ee448346d2
Ajaygupta1993 <ajaykumar.gupta04@gmail.com>
2021-08-04 14:46:46 +0530

    logger implimented

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
@@ -1,18 +1,25 @@
 package com.shopme.admin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	
+	private static final Logger LOGGER=LoggerFactory.getLogger(MainController.class);
+	private static final String START_METHOD="method statred";
 	@GetMapping("")
 	public String viewHomePage() {
+		 String methodName="viewHomePage";
+		 LOGGER.info(methodName );
 		return "index";
 	}
 	
 	@GetMapping("/login")
 	public String viewLoginPage() {
+		String methodName="viewLoginPage";
+		 LOGGER.info(methodName);
 		return "login";
 	}
 	
